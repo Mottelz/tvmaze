82d154a7095d3b32d9cc7ae99baba9147406d3c1
Mottel Zirkind <shachna@gmail.com>
2019-03-18 22:59:10 -0400

    Commit before coding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './views/search/search.component';
+import { ShowsComponent } from './views/shows/shows.component';
+import { ShowComponent } from './views/show/show.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ShowsComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
